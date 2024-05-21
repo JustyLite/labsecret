d6019498a2b91ed2dbd9fce70128ff98eebd6fd6
JustyLite <123053540+JustyLite@users.noreply.github.com>
2024-05-21 13:06:21 +0900

    фаыпвфавы

--- src/main/java/com/neuro/labsecret/LabsecretApplication.java
+++ src/main/java/com/neuro/labsecret/LabsecretApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LabsecretApplication {
-
+	//Алоало
 	//asdfasf
 	public static void main(String[] args) {
 		SpringApplication.run(LabsecretApplication.class, args);
